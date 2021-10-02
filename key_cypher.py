867e37529f5d54d26cfcaf2ea7d0cf0213df627b
Danelevsk <tiunov.danila.al@mail.ru>
2021-10-03 05:23:41 +1000

    Заточка метода под треование+новый метод

--- key_cypher.py
+++ key_cypher.py
@@ -1,60 +1,202 @@
-# import os
+import random, os
 
 
-class Encryption():
+class Encryption:
     def __init__(self, key):
+        self.Cheak_Key(key)  # Вызов метода_проверки корректности ключа
         self.key = key
+        full_path_text = 'text_1.txt'  # Здесь нужно указать на файл_текст интерфейса
+        self.full_path_text = full_path_text
 
     def AveMe(self):
-        cypher = []
-        for element in range(len(key)):
-            index = ord(key[element]) + 3 # суть шифра!!!
-            cypher.append(chr(index))
-        cypher.append(1)
-        print(key)
-        print(cypher)
-        self.Save_file(cypher)
+        '''
+        Для дешифратора:
+            Каждый элемент текста смещается на key символов по таблице ASCII.
+            Адекватные символы находятся на позициях: 32-123, 1040-1103.
+            От сюда следует зациклить перенос ползунка таблицы на 32 при
+            индексе > 1103 и на 1040 при индексе > 123, чтобы индекс не попал
+            в интервалы ...-31, 124-1039, 1104-...
+        :return: ->file
+            Название файла "Имя_файла_текса" + "_cypher.txt"
+        '''
+        # Проверка на наличие файла
+        if os.path.exists(self.full_path_text.replace('.txt', '_cypher.txt')):
+            temp_file = self.full_path_text.replace('.txt', '_cypher.txt')
+            os.remove(temp_file)
+        # Создание нового шифра (удаляя предыдущий при его наличии)
+        file_cypher = self.full_path_text.replace('.txt', '_cypher.txt')
+        # Открытие и работа с файлом_текстом
+        with open(self.full_path_text, 'r', encoding='utf-8') as file_text:
+            # Пробежка по всем строкам файла
+            for line in file_text:
+                # Строка_шифр для записи построчно в новый файл + ее обнуление
+                cypher = ''
+                # Пробежка по каждому элементу строки
+                for index in range(len(line)):
+                    # Проверка на локализацию ползунка и перенос при необходимости
+                    if ord(line[index]) + int(key) > 126 and \
+                            ord(line[index]) + int(key) < 1040:
+                        index_cypher = ord(line[index]) + int(key) + 914  # суть шифра
+                        cypher += chr(index_cypher)
+                    elif ord(line[index]) + int(key) > 1103:
+                        index_cypher = ord(line[index]) + int(key) - 1073  # суть шифра
+                        cypher += chr(index_cypher)
+                    else:
+                        index_cypher = ord(line[index]) + int(key)  # суть шифра
+                        cypher += chr(index_cypher)
+                print(cypher)
+                # Запись строки в файл
+                with open(file_cypher, "a", encoding="utf-8") as file_write:
+                    file_write.write(cypher)
+
 
     def Сaesar_Сycle(self):
-        cypher = []
-        for element in range(len(key)):
-            if (ord(key[element]) + element) > 126:
-                '''
-                диапозон доступных значений по таблице ASCII 33-126,
-                потому, если значение больше 126, то (Temp - (126 - 33)).
-                Если такое сучается, то добавить в конце индекс циклического
-                символа, в конце зафиксировать количество изменений
-                (текущее len - len_kay) и при дешифровке учесть, что
-                последний символ - номер шифра, а предыдущие за ним - номера
-                сиволов, прошедшие круг при шифровании.
-                ord(key[element]) + element - 93
-                '''
+        '''
+        Для дешифратора:
+            Каждый элемент текста смещается на key символов по таблице ASCII.
+            Адекватные символы находятся на позициях: 32-123, 1040-1103.
+            От сюда следует зациклить перенос ползунка таблицы на 32 при
+            индексе > 1103 и на 1040 при индексе > 123, чтобы индекс не попал
+            в интервалы ...-31, 124-1039, 1104-...
+            Данный шифратор отличается от AveMe тем, что на каждом вычислении
+            индекса по таблице ASCII происходит циклическое увеличение/умень-
+            шение на 1 числа сдвигов (растет до 10, затем падает до 0, затем
+            снова растет и тд.)
+        :return: ->file
+            Название файла "Имя_файла_текса" + "_cypher.txt"
+        '''
+        # Проверка на наличие файла
+        if os.path.exists(self.full_path_text.replace('.txt', '_cypher.txt')):
+            temp_file = self.full_path_text.replace('.txt', '_cypher.txt')
+            os.remove(temp_file)
+        # Создание нового шифра (удаляя предыдущий при его наличии)
+        file_cypher = self.full_path_text.replace('.txt', '_cypher.txt')
+        # Открытие и работа с файлом_текстом
+        with open(self.full_path_text, 'r', encoding='utf-8') as file_text:
+            # Пробежка по всем строкам файла
+            for line in file_text:
+                # Строка_шифр для записи построчно в новый файл + ее обнуление
+                cypher = ''
+                cycle_index = 0
+                tumbler = 1
+                # Пробежка по каждому элементу строки
+                for index in range(len(line)):
+                    # Проверка циклического индекса и тумблера на рост/уменьшение
+                    if cycle_index <= 9 and tumbler == 1:
+                        # Проверка на локализацию ползунка и перенос при необходимости
+                        if 126 < ord(line[index]) + int(key) + cycle_index < 1040:
+                            index_cypher = ord(line[index]) + int(key) + 914 \
+                                           + cycle_index  # суть шифра
+                            cypher += chr(index_cypher)
+                        elif ord(line[index]) + int(key) > 1103:
+                            index_cypher = ord(line[index]) + int(key) - 1073 \
+                                           + cycle_index  # суть шифра
+                            cypher += chr(index_cypher)
+                        else:
+                            index_cypher = ord(line[index]) + int(key) \
+                                           + cycle_index  # суть шифра
+                            cypher += chr(index_cypher)
+                        # Условие роста циклического индекса
+                        cycle_index += 1
+                    # Если индекс > 9, то тумблер в 0 и начало уменьшения индекса
+                    else:
+                        tumbler = 0
+                        # Проверка на локализацию ползунка и перенос при необходимости
+                        if 126 < ord(line[index]) + int(key) + cycle_index < 1040:
+                            index_cypher = ord(line[index]) + int(key) + 914 \
+                                           + cycle_index  # суть шифра
+                            cypher += chr(index_cypher)
+                        elif ord(line[index]) + int(key) > 1103:
+                            index_cypher = ord(line[index]) + int(key) - 1073 \
+                                           + cycle_index  # суть шифра
+                            cypher += chr(index_cypher)
+                        else:
+                            index_cypher = ord(line[index]) + int(key) \
+                                           + cycle_index  # суть шифра
+                            cypher += chr(index_cypher)
+                        # Условие уменьшения циклического индекса
+                        cycle_index -= 1
+                    # Если циклический индекс упал до 0, то пора начинать сначала
+                    if tumbler == 0 and cycle_index == 0:
+                        tumbler = 1
+                print(cypher)
+                # Запись строки в файл
+                with open(file_cypher, "a", encoding="utf-8") as file_write:
+                    file_write.write(cypher)
 
-            index = ord(key[element]) + element  # суть шифра!!!
-            cypher.append(chr(index))
-        cypher.append(2)
+#------------------Multi_Cucle метод не готов---------------------
+    def Multi_Cucle(self):
+        '''
+        Для дешифратора:
+            Каждый элемент ключа смещается на 1 символ по таблице ASCII,
+            если текущий элемент и следующий за ним - цифры, то они
+            шифруются в 16ричной системе счисления, если следующий элемент
+            не цифра, то текущий просто смещается на 20 индексов по таблице
+            ASCII. При этом для парных цифр фиксируется индекс с установкой
+            одной из букв A-Z, а для одинарных a-z.
+            В конце ставится номер шифра, в данном случае №3.
+        Рс. Необходимо придумать алгоритм распознавания длины ключа, так
+            как она может быть больше 9 и составлять двузначное число.
+        :return: -> list
+            ключ_шифр+(a-z+индексы/A-Z+индекс)+...+Len_key+№_шифратора
+        '''
+        cypher = []
+        queue = ''
+        for element in range(len(key)):
+            if 47 < ord(key[element]) < 58:
+                # Условие для двух цифр подрят
+                if ((element + 2) <= len(key)) and (47 < ord(key[element + 1]) < 58):
+                    index = str(key[element]) + str(key[element + 1])
+                    # Условие для 0xс в 0х0с
+                    if int(index) < 15:
+                        index = hex(int(index))
+                        exception = ''
+                        exception = str(index[0] + index[1]) + '0' + str(index[2])
+                        cypher.append(exception)
+                    else:
+                        cypher.append(hex(int(index)))
+                    rand_ASCII = random.randint(65, 90)
+                    queue += chr(rand_ASCII) + str(element)
+                else:
+                    index = ord(key[element]) + 20  # суть шифра
+                    cypher.append(chr(index))
+                    rand_ASCII = random.randint(97, 122)
+                    queue += chr(rand_ASCII) + str(element)
+            else:
+                index = ord(key[element]) + 1  # суть шифра
+                cypher.append(chr(index))
+        cypher.append('~')
+        cypher.append(str(queue))
+        cypher.append(3)
         print(key)
         print(cypher)
         self.Save_file(cypher)
 
-    def Save_file(self, cypher):
-        file_cypher = full_path.replace('.txt', '_cypher.txt')
-        str_cypher = ''
-        for index in range(len(cypher)):
-            str_cypher += str(cypher[index])
-        print(str_cypher)
-        with open(file_cypher, "w", encoding="utf-8") as file_write:
-            file_write.write(str_cypher)
+
+    @staticmethod
+    def Cheak_Key(test_key):
+        for index in range(len(test_key)):
+            if ord(test_key[index]) > 57 or ord(test_key[index]) < 48:
+                raise TypeError("Ключ должен быть целым числом")
+        if int(test_key) <= 0 or int(test_key) > 150:
+            raise ValueError("Ключ должен быть числом от 0 до 150. "
+                             "Большее значение даст нерациональную нагрузку"
+                             " на ЦП, а алгоритм зациклен на 150 единицах")
 
 
-full_path = 'key_1.txt'
-('При наличии пути к файлу (вне текущего каталога)\n'
- 'раскомментить 3 строки ниже и удалить строку выше\n'
- 'dir = ''C:/Users/xxx'' \n'
- 'key_file = ''key_1.txt'' \n'
- 'full_path = os.path.join(dir, key_file) \n')
-with open(full_path, 'r', encoding='utf-8') as file_key:
+full_path_key = 'key_1.txt'
+'''
+При наличии пути к файлу (вне текущего каталога)
+раскомментить 3 строки ниже и удалить строку выше
+dir = ''C:/Users/xxx''
+key_file = ''key_1.txt''
+full_path = os.path.join(dir, key_file)
+Добавить "import os" в начале.
+Сделать так для обоих файлов full_path_text = 'text_1.txt'
+'''
+with open(full_path_key, 'r', encoding='utf-8') as file_key:
     key = file_key.readline()
-
 temp = Encryption(key)
-temp.AveMe()
+#  temp.AveMe()
+temp.Сaesar_Сycle()
+# temp.Multi_Cucle()
